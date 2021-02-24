ec2aaf4d3462283e4f97b7c9eb3bf10c108a4a3b
권민철 <luffy@woowahan.com>
2021-02-24 20:17:02 +0900

    init project

---
+++ src/test/java/com/woowahan/mongostudy/MongostudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowahan.mongostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongostudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
